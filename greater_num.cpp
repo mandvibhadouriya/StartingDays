32fae7d79a828df80c379ec74dd9b4a50c4db841
manuthakur0909 <thakur.manu0909@gmail.com>
2021-05-03 10:08:44 +0530

    Second program

---
+++ greater_num.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+cout<<"Give value of a ";cin>>a;
+cout<<"\nGive value of b ";cin>>b;
+if(a>b){
+cout<<"A is greater\n";
+}
+else{
+cout<<"B is greater\n";
+}
